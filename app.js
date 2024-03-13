f9fd10606f11f493106325533356754a1692c0f7
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-03-13 15:28:37 -0400

    Geting it done

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ server.listen(3000, () => {
 
 
 
+const express = require("express")
+const app = express()
+const port = 3000;
+
+app.get("/", (req,res) => {
+    res.send("Hello World")
+})
+
+
 
 
 
